721ca744ed5504f0b89372ba163dcb73937384d0
k3yboardnerd <58972426+k3yboardnerd@users.noreply.github.com>
2023-12-01 19:52:01 +0200

    Update apiSlice.js

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://kbnblog-api.onrender.com'
+  baseUrl: ''
 })
 
 export const apiSlice = createApi({

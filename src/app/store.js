4b4325517d7810938ee89f2a6559e98e834dcc15
k3yboardnerd <k3yboardnerd@gmail.com>
2023-12-01 16:37:21 +0200

    feature: switching devtools off

--- src/app/store.js
+++ src/app/store.js
@@ -16,5 +16,5 @@ export const store = configureStore({
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
-  devTools: true
+  devTools: false
 })
